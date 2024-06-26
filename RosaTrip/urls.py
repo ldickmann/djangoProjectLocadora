bf8badced56748862382de0595655eebca38e450
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-06-26 18:38:04 -0300

    Atualizações para bootstrap v5.3 e modificações nas linhas do HTML e CSS index, pois foi necessário devido a falta d eresponsividade

--- RosaTrip/urls.py
+++ RosaTrip/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 # Criação das rotas, através do método path, que direcionam as requisições para as views correspondentes.
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index.html'),
     path('accounts/login/', views.login, name='login'),
     path('accounts/cadastro/', views.cadastro, name='cadastro'),
     path('veiculos/', views.lista_veiculos, name='veiculos'),
