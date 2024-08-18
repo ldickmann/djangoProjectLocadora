9a1853bb5d27834f42d6541ea0ddc092f8a6a5b2
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-08-18 18:35:56 -0300

    Atualizações ursl.py e outros templates

--- RosaTrip/views.py
+++ RosaTrip/views.py
@@ -1,7 +1,7 @@
 from RosaTrip.models import Veiculo
 from RosaTrip.forms import VeiculoModelForm
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
@@ -31,9 +31,31 @@ class VeiculosListView(ListView):
         return veiculos
 
 
+class VeiculoDetailView(DetailView):
+    model = Veiculo
+    template_name = 'detalhe.html'
+
+
 @method_decorator(login_required(login_url='login'), name='dispatch')
 class VeiculoCreateView(CreateView):
     model = Veiculo
     form_class = VeiculoModelForm
     template_name = 'add-veiculo.html'
+    success_url = '/veiculos/'
+
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class VeiculoUpdateView(UpdateView):
+    model = Veiculo
+    form_class = VeiculoModelForm
+    template_name = 'edit-veiculo.html'
+
+    def get_success_url(self):
+        return reverse_lazy('detalhe', kwargs={'pk': self.object.pk})
+
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class VeiculoDeleteView(DeleteView):
+    model = Veiculo
+    template_name = 'del-veiculo.html'
     success_url = '/veiculos/'
--- djangoProjectRosa/urls.py
+++ djangoProjectRosa/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
-from RosaTrip.views import VeiculoCreateView, VeiculosListView, home, contato
+from django.urls import path
+import RosaTrip.views
 from accounts.views import register_view, login_view, logout_view
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,11 +8,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home, name='index'),
-    path('contato/', contato, name='contato'),
+    path('home/', RosaTrip.views.home, name='index'),
+    path('contato/', RosaTrip.views.contato, name='contato'),
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
-    path('veiculos/', VeiculosListView.as_view(), name='frota'),
-    path('add-veiculo/', VeiculoCreateView.as_view(), name='add-veiculo'),
+    path('veiculos/', RosaTrip.views.VeiculosListView.as_view(), name='frota'),
+    path('add-veiculo/', RosaTrip.views.VeiculoCreateView.as_view(), name='add-veiculo'),
+    path('veiculo/<int:pk>/', RosaTrip.views.VeiculoDetailView.as_view(), name='detalhe'),
+    path('veiculo/<int:pk>/editar/', RosaTrip.views.VeiculoUpdateView.as_view(), name='edit-veiculo'),
+    path('veiculo/<int:pk>/deletar/', RosaTrip.views.VeiculoDeleteView.as_view(), name='del-veiculo'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
