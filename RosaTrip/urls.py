4fcd7af2c7d219ab636c40fa8a29d334bcdd062b
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-08-19 06:21:21 -0300

    Update urls.py

--- RosaTrip/urls.py
+++ RosaTrip/urls.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 
 
-# Criação das rotas, através do método path, que direcionam as requisições para as views correspondentes.
 class RosatripConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'RosaTrip'
